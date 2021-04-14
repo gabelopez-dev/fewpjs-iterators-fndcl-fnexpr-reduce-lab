4221c947b248bbac20c0ee57b57c2b0229c7acbe
Gabriel Lopez <gabelopez84@gmail.com>
2021-04-14 12:26:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+totalBatteries.reduce(function(sum, Batteries) { totalBatteries.assign(batteryBatches)(totalBatteries)}, hogwartsHouses) 
+
+batteryBatches["5"] //=> [hermioneGranger, ronWeasley, harryPotter] 
